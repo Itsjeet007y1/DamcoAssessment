8fe393b7af31bb44fb061edf43c85974d4fb74e9
91840 <91840@LAPTOP-FP6KOQOO>
2021-07-15 18:27:16 +0530

    Added input validations, exception handling, and other changes

--- DamcoAssessment/src/main/java/com/example/demo/DamcoAssessmentApplication.java
+++ DamcoAssessment/src/main/java/com/example/demo/DamcoAssessmentApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -24,5 +26,15 @@ public class DamcoAssessmentApplication {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("com.example.demo")).build();
 	}
+	
+	@Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
 
 }
--- DamcoAssessment/src/main/java/com/example/demo/controller/UserController.java
+++ DamcoAssessment/src/main/java/com/example/demo/controller/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,8 +14,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.ResponseDefObject;
 import com.example.demo.model.User;
 import com.example.demo.service.IUserService;
+import com.example.demo.utility.Util;
 
 @RestController
 public class UserController {
@@ -24,20 +28,58 @@ public class UserController {
 	IUserService userService;
 	
 	@PostMapping("/saveuser")
-	public User createUser(@RequestBody User user) {
-		logger.info("saveuser end point is called.");
-		return userService.createUser(user);
+	public ResponseEntity<ResponseDefObject<User>> createUser(@RequestBody User user) {
+		try {
+			logger.info("saveuser end point is called.");
+			User savedUser = userService.createUser(user);
+			return new ResponseEntity<ResponseDefObject<User>>(new ResponseDefObject<User> 
+			(HttpStatus.CREATED.value(), Util.SUCCESS, savedUser), HttpStatus.ACCEPTED);
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			return new ResponseEntity<ResponseDefObject<User>>(new ResponseDefObject<User> 
+			(HttpStatus.EXPECTATION_FAILED.value(), Util.ERROR, e.getMessage()), HttpStatus.EXPECTATION_FAILED);
+		}
+	}
+	
+	@PostMapping("/updateuser")
+	public ResponseEntity<ResponseDefObject<User>> updateUser(@RequestBody User user) {
+		try {
+			logger.info("updateuser end point is called.");
+			User savedUser = userService.createUser(user);
+			return new ResponseEntity<ResponseDefObject<User>>(new ResponseDefObject<User> 
+			(HttpStatus.CREATED.value(), Util.SUCCESS, savedUser), HttpStatus.ACCEPTED);
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			return new ResponseEntity<ResponseDefObject<User>>(new ResponseDefObject<User> 
+			(HttpStatus.EXPECTATION_FAILED.value(), Util.ERROR, e.getMessage()), HttpStatus.EXPECTATION_FAILED);
+		}
 	}
 	
 	@GetMapping("/getusers")
-	public List<User> getUsers() {
-		logger.info("getusers end point is called.");
-		return userService.readUser();
+	public ResponseEntity<ResponseDefObject<List<User>>> getUsers() {
+		try {
+			logger.info("getusers end point is called.");
+			List<User> savedUsersList = userService.readUser();
+			return new ResponseEntity<ResponseDefObject<List<User>>>(new ResponseDefObject<List<User>> 
+			(HttpStatus.CREATED.value(), Util.SUCCESS, savedUsersList), HttpStatus.ACCEPTED);
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			return new ResponseEntity<ResponseDefObject<List<User>>>(new ResponseDefObject<List<User>> 
+			(HttpStatus.EXPECTATION_FAILED.value(), Util.ERROR, e.getMessage()), HttpStatus.EXPECTATION_FAILED);
+		}
 	}
 	
 	@DeleteMapping("/deleteuser/{id}")
-	public String deleteUser(@PathVariable("id") String id) {
-		logger.info("deleteuser end point is called.");
-		return userService.deleteUser(id);
+	public ResponseEntity<ResponseDefObject<String>> deleteUser(@PathVariable("id") String id) {
+		try {
+			logger.info("deleteuser end point is called.");
+			String str = userService.deleteUser(id);
+			return new ResponseEntity<ResponseDefObject<String>>(new ResponseDefObject<String> 
+			(HttpStatus.CREATED.value(), Util.SUCCESS, str), HttpStatus.ACCEPTED);
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			return new ResponseEntity<ResponseDefObject<String>>(new ResponseDefObject<String> 
+			(HttpStatus.EXPECTATION_FAILED.value(), Util.ERROR, e.getMessage()), HttpStatus.EXPECTATION_FAILED);
+		}
 	}
 }
